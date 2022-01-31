d998244ef17e540f3319f3ed7c4bd2c44132924c
Khmarigou <luca.plc@outlook.fr>
2022-01-31 09:19:04 +0100

    Création fichiers MVC 2

---
+++ MVC/view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>DVD Share</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body>
+	<header>
+		
+	</header>
+
+	<footer>
+		
+	</footer>
+</body>
+</html>
