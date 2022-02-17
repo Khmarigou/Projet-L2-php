55c7a7f2bb4b165fe0f361377867394f62efad0b
Khmarigou <luca.plc@outlook.fr>
2022-02-17 10:45:08 +0100

    test

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$c = mysqli_connect("localhost", "l2", "L2", "CMIndustries");
-mysqli_set_charset($c, "utf8");	
+$c = mysqli_connect("os-vps418.infomaniak.ch", "l2_info_11", "Mei9shoh", "User");
+mysqli_set_charset($c, "utf8");
 ?>
