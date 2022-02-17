39c0efa7f9545aff88591cd81db217fb125e2614
Emilien39110 <emilien.boitouzet@hotmail.fr>
2022-02-17 11:54:11 +0100

    connection

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 //$c = mysqli_connect("localhost", "l2", "L2", "INFO405");
-$c = mysqli_connect("os-vps418.infomaniak.ch:1180", "l2_info_11", "Mei9shoh", "l2_info_11");
+$c = mysqli_connect("os-vps418.infomaniak.ch", "l2_info_11", "Mei9shoh", "User");
 mysqli_set_charset($c, "utf8");
 ?>
