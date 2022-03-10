9b8b166e5a5545cd60a32d7772927da03ac4fbff
Khmarigou <luca.plc@outlook.fr>
2022-03-10 09:26:22 +0100

    Ajout connexion dans louer

--- PAGES/louer.php
+++ PAGES/louer.php
@@ -1,12 +1,13 @@
 <?php
     creer_table_dvd();
-    //if(isset($_SESSION["username"])){
+    if(isset($_SESSION["username"])){
        posterAnnonce();
-    // }else{
-    //    echo "<div class='location'>";
-    //    echo "<h2>Vous devez être connecté(e) pour pouvoir louer vos DVD</h2>";
-    //    echo "</div>";
-    //} 
+    }else{
+       echo "<div class='location'>";
+       echo "<h2>Vous devez être connecté(e) pour pouvoir louer vos DVD</h2>";
+       echo "<a href='.?page=connexion'><li>Cliquer ici pour vous connecter</li></a>";
+       echo "</div>";
+    } 
     
 
 ?>
