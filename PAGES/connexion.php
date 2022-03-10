744ab52162d44d5f54ea6509e414c6207b562e1e
alexandredesbos <82172155+alexandredesbos@users.noreply.github.com>
2022-03-10 11:02:04 +0100

    ajout balise pour css

--- PAGES/connexion.php
+++ PAGES/connexion.php
@@ -5,17 +5,20 @@ if(isset($_SESSION["username"])){
 }
 ?>
 
-	<form action="../praujet/MVC/modele.php" method="POST">
-                <h2>Connexion</h2>
-                
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+    <section>
+        <form action="../praujet/MVC/modele.php" method="POST">
+                    <h2>Connexion</h2>
+                    
+                    <label><b>Nom d'utilisateur</b></label>
+                    <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+                    <br>
+                    <label><b>Mot de passe</b></label>
+                    <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+                    <br>
+                    <input type="submit" name='login' >
+        </form>
 
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+        <p>Vous n'avez pas de compte ? <a href=".?page=inscription">Inscrivez vous</a></p>
 
-                <input type="submit" name='login' >
-    </form>
-
-    <p>Vous n'avez pas de compte ? <a href=".?page=inscription">Inscrivez vous</a></p>
+    </section>
 </section>
