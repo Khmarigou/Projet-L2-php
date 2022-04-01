454a4e6d4b44912b6267f8309bbef98817f28c89
alexandredesbos <82172155+alexandredesbos@users.noreply.github.com>
2022-04-01 23:00:49 +0200

    opti

--- MODEL/modele.php
+++ MODEL/modele.php
@@ -3,24 +3,24 @@
 <?php
 
 global $c;
-// $sql = "CREATE TABLE User(
-//     id INT NOT NULL AUTO_INCREMENT,
-//     nom VARCHAR(50) NOT NULL,
-// 	prenom VARCHAR(50) NOT NULL,
-// 	username VARCHAR(50) NOT NULL,
-// 	password VARCHAR(50) NOT NULL,
-// 	is_admin TINYINT,
+$sql = "CREATE TABLE User(
+    id INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+	prenom VARCHAR(50) NOT NULL,
+	username VARCHAR(50) NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	is_admin TINYINT,
 
-//     CONSTRAINT Pk_Dvd PRIMARY KEY (id))";
+    CONSTRAINT Pk_Dvd PRIMARY KEY (id))";
  
-// $result = mysqli_query($c, $sql); 
+$result = mysqli_query($c, $sql); 
 
 
 
 if(isset($_POST["login"])){
 	session_start();
 	if(!empty($_POST['username']) AND !empty($_POST['password']))
-	{
+	{	
 		$db = mysqli_connect("localhost:3306", "root", "", "l2_info_11");
 		$username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
 		$password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
@@ -72,23 +72,6 @@ if(isset($_POST["register"])){
 		header('Location: ../index.php?page=inscription&error=2');
 	}
 }
-
-// if(isset($_POST["register"])){
-// 	session_start();
-// 	if(!empty($_POST['username']) AND !empty($_POST['password']) AND !empty($_POST['name']) AND !empty($_POST['surname']))
-// 	{
-// 		$db = mysqli_connect("localhost:3306", "l2_info_11", "Mei9shoh", "l2_info_11");
-// 		$sql = "INSERT INTO `User` (`id`, `nom`, `prenom`, `username`, `password`, `is_admin`) VALUES (NULL,'$_POST[surname]', '$_POST[name]', '$_POST[username]', '$_POST[password]', 0);";
-// 		$results = mysqli_query($db,$sql);
-
-// 		header('Location: ../index.php?page=connexion');
-// 	}
-// 	else
-// 	{
-// 		header('Location: ../index.php?page=inscription&error=1');
-// 	}
-// }
-
 
 function afficher_admin()
 {	
