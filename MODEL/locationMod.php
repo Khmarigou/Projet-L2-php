10f13bbb72a49ccef6da2ed12dfc61246771a848
Emilien39110 <emilien.boitouzet@hotmail.fr>
2022-04-28 16:00:51 +0200

    modification

--- MODEL/locationMod.php
+++ MODEL/locationMod.php
@@ -1,14 +1,14 @@
 <?php
 
 //var_dump("coucou");
-// function louer(){
-// 	global $c ;
+if(isset($_POST["login"])){
+	global $c ;
 
-// 	$id = $_POST["louer"];
-// 	$sql = "UPDATE Dvd SET dispo = 0 WHERE id = $id";
-// //var_dump($sql);
-// 	$result = mysqli_query($c,$sql);
-// //var_dump($result); exit;
-// 	header('Location: ../index.php?page=suggestion');
-// }
+	$id = $_POST["louer"];
+	$sql = "UPDATE Dvd SET dispo = 0 WHERE id = $id";
+//var_dump($sql);
+	$result = mysqli_query($c,$sql);
+//var_dump($result); exit;
+	header('Location: ../index.php?page=suggestion');
+}
 ?>
