029bf330d09f917f787928a048d64a3a3a0b8ab2
Emilien39110 <emilien.boitouzet@hotmail.fr>
2022-04-28 16:07:24 +0200

    Update db.php

--- db.php
+++ db.php
@@ -8,10 +8,10 @@
 
 
 //Base Serveur
-//$c = mysqli_connect("localhost", "l2_info_11", "Mei9shoh", "l2_info_11");
+$c = mysqli_connect("localhost", "l2_info_11", "Mei9shoh", "l2_info_11");
 
 //Base Serveur
-$c = mysqli_connect("localhost", "l2_info_11", "Mei9shoh", "l2_info_11");
+//$c = mysqli_connect("localhost", "l2_info_11", "Mei9shoh", "l2_info_11");
 mysqli_set_charset($c, "utf8");
 
 ?>
