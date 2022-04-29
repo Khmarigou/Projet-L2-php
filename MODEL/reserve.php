21597d591ce2fe766b8f3f40adc3a7e8910da5d4
Emilien39110 <emilien.boitouzet@hotmail.fr>
2022-04-29 15:35:40 +0200

    Update reserve.php

--- MODEL/reserve.php
+++ MODEL/reserve.php
@@ -1,11 +1,14 @@
 
 <?php
+session_start();
+
 $db = mysqli_connect("localhost", "l2_info_11", "Mei9shoh", "l2_info_11");
 if(isset($_POST["location"])){
     $deb = $_POST['debut'];
     $fin = $_POST['fin'];
     $idDvd = $_POST['idDvd'];
-        $sql = "INSERT INTO Reservation (idDvd, idLocataire, dateDebut, dateFin) VALUES ($idDvd,1,'$deb','$fin')";
+    $idUser = $_SESSION['idUser']
+        $sql = "INSERT INTO Reservation (idDvd, idLocataire, dateDebut, dateFin) VALUES ($idDvd,$idUser,'$deb','$fin')";
         $result = mysqli_query($db, $sql);
         var_dump($deb); var_dump($fin);
         var_dump($sql);var_dump($result);
