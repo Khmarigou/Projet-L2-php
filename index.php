dad26a31d8177c71922eb31b378394851e0c3f35
Emilien39110 <emilien.boitouzet@hotmail.fr>
2022-04-29 12:35:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once "db.php";
 include_once "MODEL/modele.php";
 
 include_once "MODEL/louerModel.php";
-include_once "MODEL/locationMod.php";
+include_once "MODEL/reserve.php";
 include_once "MODEL/points.php";
 include_once "MODEL/reservation.php";
 
