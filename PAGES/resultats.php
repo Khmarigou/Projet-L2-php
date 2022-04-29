fb3b27a1c24b63a8e7c2c33c78776f706de2cdc0
alexandredesbos <82172155+alexandredesbos@users.noreply.github.com>
2022-04-29 14:41:37 +0200

    correction

--- PAGES/resultats.php
+++ PAGES/resultats.php
@@ -4,7 +4,7 @@
 	<div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="./IMAGES/banniere.jpg">
         <form class="d-flex tm-search-form" action="./?page=resultats" method="POST" autocomplete="off">
 			<?php
-				$sql = "SELECT titre FROM dvd";
+				$sql = "SELECT titre FROM Dvd";
 				$listeTitres = recup_dvd_sql ($sql);
 				foreach ($listeTitres as $key => $value) {
 					$Titres[] = $value['titre'];
