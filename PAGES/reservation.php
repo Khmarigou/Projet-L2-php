51979b82a59677e486ae8551e5c8bf2767f4e705
Emilien39110 <emilien.boitouzet@hotmail.fr>
2022-04-29 14:52:33 +0200

    Update reservation.php

--- PAGES/reservation.php
+++ PAGES/reservation.php
@@ -16,8 +16,9 @@ $sql = "CREATE TABLE Reservation(
 
 if(isset($_GET['id'])){
     $id = $_GET['id'];
+    var_dump($id); exit;
         echo "<form method='POST' action='MODEL/reserve.php' enctype='multipart/form-data' value='id'>";
-        echo "<input type='text' name='id' value='$id' disabled='disabled'/></br>";
+        echo "<input type='text' name='idDvd' value='$id' disabled='disabled'/></br>";
         echo "<label>Date de début : </label>";
         echo "<input type='date' name='debut'/></br>";
         echo "<label>Date de Fin : </label>";
