6a1e16b5f34b928d8f014ba9018061e45955932d
Emilien39110 <emilien.boitouzet@hotmail.fr>
2022-04-29 14:32:02 +0200

    reservation

--- MODEL/reserve.php
+++ MODEL/reserve.php
@@ -4,9 +4,13 @@ $db = mysqli_connect("localhost", "l2_info_11", "Mei9shoh", "l2_info_11");
 if(isset($_POST["location"])){
     $deb = $_POST['debut'];
     $fin = $_POST['fin'];
-    $sql = "INSERT INTO Reservation (idDvd, idLocataire, dateDebut, dateFin) VALUES (10,1,'$deb','$fin')";
-    $result = mysqli_query($db, $sql);
-    var_dump($deb); var_dump($fin);
-    var_dump($sql);var_dump($result);
+    if(isset($_GET['id'])){
+        $idDvd = $_GET['id'];
+        $sql = "INSERT INTO Reservation (idDvd, idLocataire, dateDebut, dateFin) VALUES ($idDvd,1,'$deb','$fin')";
+        $result = mysqli_query($db, $sql);
+        var_dump($deb); var_dump($fin);
+        var_dump($sql);var_dump($result);
+    }
+    
 }
 ?>
