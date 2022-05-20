f0c0860db03a0511302eed2c8fbc2ddc02b4eda4
Emilien39110 <emilien.boitouzet@hotmail.fr>
2022-05-20 15:53:14 +0200

    bdd

--- MODEL/modele.php
+++ MODEL/modele.php
@@ -3,7 +3,7 @@
 <?php
 
 //$c = mysqli_connect("localhost", "root", "", "l2_info_11");
-$c = mysqli_connect("localhost:3307", "l2_info_11", "Mei9shoh", "l2_info_11");
+$c = mysqli_connect("localhost", "l2_info_11", "Mei9shoh", "l2_info_11");
 
 $sql = "CREATE TABLE User(
     idUser INT NOT NULL AUTO_INCREMENT,
