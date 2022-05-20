c3248386db65290ebf381aa644943f87974af055
alexandredesbos <82172155+alexandredesbos@users.noreply.github.com>
2022-05-20 16:12:00 +0200

    Update profil.php

--- PAGES/profil.php
+++ PAGES/profil.php
@@ -1,21 +1,3 @@
- <?php
-    
-    if(isset($_SESSION["username"])){
-
-        echo "<h2 id='bonjour'>Bonjour " . $_SESSION["username"] .", vous êtes connecté.</h2>";
-
-        echo "<li class='nav-item'><a href='index.php?page=logView'>Activités</a></li>";
-        echo "<li class='nav-item'><a href='index.php?page=compte'>Mes informations</a></li>";
-
-        if($_SESSION["is_admin"] == 1){
-            echo "<li class='nav-item'><a href='index.php?page=admin'>Modération</a></li>";
-        }
-        
-    }else{
-        echo "Vous devez être connecté(e).";
-    }
-    
-    ?>
 <section class='pageProfil'>
 
    
