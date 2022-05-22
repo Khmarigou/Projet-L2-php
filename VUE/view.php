5c29d0daff9296738ecb92d29ea32f4478cb366c
alexandredesbos <82172155+alexandredesbos@users.noreply.github.com>
2022-05-22 10:40:53 +0200

    Update view.php

--- VUE/view.php
+++ VUE/view.php
@@ -73,13 +73,13 @@
             <div class="row">
                 <div class="col-lg-6 col-md-12 col-12 px-5 mb-5">
                     <h3 class="tm-text-primary mb-4 tm-footer-title">About DVD Share</h3>
-                    <p>DVD Share est une plateforme en ligne de location de DVD entre particulier.</p>
+                    <p>DVD Share est une plateforme en ligne de location de DVD entre particulier. Les locations sont basé sur un système de point qui donne la priorité au utilisateurs les plus engagés.</p>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
                     <h3 class="tm-text-primary mb-4 tm-footer-title">Liens utiles</h3>
                     <ul class="tm-footer-links pl-0">
-                        <li><a href="#">Support</a></li>
-                        <li><a href=".?page=connexion">Connexion</a></li>
+                        <li><a href=".?page=suggestion">Explorer</a></li>
+                        <li><a href=".?page=louer">Louer</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
@@ -89,8 +89,7 @@
                         <li class="mb-2"><a href="https://instagram.com"><i class="fab fa-instagram"></i></a></li>
                         <li class="mb-2"><a href="https://pinterest.com"><i class="fab fa-pinterest"></i></a></li>
                     </ul>
-                    <a href="#" class="tm-text-gray text-right d-block mb-2">Terms of Use</a>
-                    <a href="#" class="tm-text-gray text-right d-block">Privacy Policy</a>
+                    
                 </div>
             </div>
             <div class="row">
