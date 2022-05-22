78a3e9e7fd6b84d0197a776fd2d5bf19fd5f20c0
Christradja <97879513+Christradja@users.noreply.github.com>
2022-05-22 13:09:14 +0200

    modif mdp

--- MODEL/logs.php
+++ MODEL/logs.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$c = mysqli_connect("localhost", "l2_info_11", "Mei9shoh", "l2_info_11");
+$c = mysqli_connect("localhost:3307", "l2_info_11", "Mei9shoh", "l2_info_11");
 
 $sql = "CREATE TABLE Logs(
     idLog INT NOT NULL AUTO_INCREMENT,
